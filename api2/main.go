d6aeb6c7a17acead78649a8e1b364dd1d4a0b0a8
SuryaV26 <vsurya245@gmail.com>
2024-06-26 20:26:37 +0530

    AP2 - Broken Authentication

--- api2/main.go
+++ api2/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+var flag = "flag{AP2-Broken_Authentication}"
+
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.New("login").Parse(`
+        <html>
+        <body>
+            <form method="POST" action="/login">
+                Username: <input type="text" name="username"><br>
+                Password: <input type="password" name="password"><br>
+                <input type="submit" value="Login">
+            </form>
+        </body>
+        </html>
+    `)
+	tmpl.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if pass, ok := users[username]; ok && pass == password {
+		fmt.Fprintf(w, "Welcome %s! Here is your flag: %s", username, flag)
+	} else {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", loginPage)
+	http.HandleFunc("/login", login)
+
+	fmt.Println("Server starting on :8002")
+	log.Fatal(http.ListenAndServe(":8002", nil))
+}
